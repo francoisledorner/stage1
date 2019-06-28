383ad548238e867d64a4d63486025bb4f36eaf35
francoisledorner <francoisledorner@hotmail.com>
2019-06-28 15:43:59 +0200

    mathjs

--- src/components/GraphPloty.vue
+++ src/components/GraphPloty.vue
@@ -2,16 +2,32 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <button @click="callIt()">Click me!</button>
+
+    <Plotly :data="donnees" :layout="layout" :display-mode-bar="false"></Plotly>
   </div>
 </template>
 
 <script>
+import { Plotly } from "vue-plotly";
+import mathjs from "import";
+
 export default {
   name: "GraphPloty",
-  mounted() {},
+  components: {
+    Plotly
+  },
   data() {
     return {
-      msg: "GraphPloty"
+      donnees: [
+        {
+          x: [1, 2, 3, 4],
+          y: [10, 15, 13, 17],
+          type: "scatter"
+        }
+      ],
+      layout: {
+        title: "My graph"
+      }
     };
   },
   methods: {
